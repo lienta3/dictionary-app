e50dc2d50bb8dd141a48e24d1cc59d4c6a833b12
lienta3 <lien.ta3@gmail.com>
2026-02-16 20:34:55 +1100

    Set up dictionary search engine

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header"></header>
-      <a href="#" className="btn btn-primary">
-        hello
-      </a>
+      <main>
+        <Dictionary />
+      </main>
+      <footer>Coded by Lien Ta</footer>
     </div>
   );
 }
